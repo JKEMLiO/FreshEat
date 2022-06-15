f046ef87a137356439a90e096ca8a952a66576aa
EMLiO <emil@cs.colman.ac.il>
2022-06-15 18:25:26 +0300

    First buttons

---
+++ FreshEatIOS/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  FreshEatIOS
+//
+//  Created by Emil Kollek on 15/06/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func btnLogin(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
