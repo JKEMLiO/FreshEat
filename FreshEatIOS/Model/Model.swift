b93e671f24558fc257eeb1209f6a54c7bd8482af
Shimon <shimoco7@cs.colman.ac.il>
2022-06-24 17:16:00 +0300

    added DataModel for CoreData and FB conf files

---
+++ FreshEatIOS/Model/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  FreshEatIOS
+//
+//  Created by Shiri Shmuely on 24/06/2022.
+//
+
+import Foundation
+
+class Model{
+    static let instance = Model()
+    
+    private init(){}
+}
---
+++ FreshEatIOS/Model/ModelFirebase.swift
@@ -0,0 +1,13 @@
+//
+//  ModelFirebase.swift
+//  FreshEatIOS
+//
+//  Created by Shiri Shmuely on 24/06/2022.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class ModelFirebase{
+    
+}
