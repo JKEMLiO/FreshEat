e3dc80da94053140912ab46a927a51ba801f63d9
JKEMLiO <74857082+JKEMLiO@users.noreply.github.com>
2022-08-13 16:12:39 +0300

    Register, profile, change password design

--- FreshEatIOS/UserProfileViewController.swift
+++ FreshEatIOS/UserProfileViewController.swift
@@ -1,21 +1,21 @@
 //
-//  RegisterViewController.swift
+//  UserProfileViewController.swift
 //  FreshEatIOS
 //
-//  Created by csuser on 02/08/2022.
+//  Created by csuser on 13/08/2022.
 //
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class UserProfileViewController: UIViewController {
 
     @IBOutlet weak var profileImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         profileImage.layer.cornerRadius=40
         profileImage.clipsToBounds=true
-        
-        
+
+        // Do any additional setup after loading the view.
     }
     
 
--- FreshEatIOS/changePasswordViewController.swift
+++ FreshEatIOS/changePasswordViewController.swift
@@ -1,21 +1,18 @@
 //
-//  RegisterViewController.swift
+//  changePasswordViewController.swift
 //  FreshEatIOS
 //
-//  Created by csuser on 02/08/2022.
+//  Created by csuser on 13/08/2022.
 //
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class changePasswordViewController: UIViewController {
 
-    @IBOutlet weak var profileImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        profileImage.layer.cornerRadius=40
-        profileImage.clipsToBounds=true
-        
-        
+
+        // Do any additional setup after loading the view.
     }
     
 
