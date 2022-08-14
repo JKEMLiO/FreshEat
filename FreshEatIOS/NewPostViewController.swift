056241a57895001acf1339ead6dce6e5132b853b
EMLIOJK <emil@cs.colman.ac.il>
2022-08-14 19:58:02 +0300

    V2

--- FreshEatIOS/NewPostViewController.swift
+++ FreshEatIOS/NewPostViewController.swift
@@ -69,6 +69,8 @@ class NewPostViewController: UIViewController, UITextViewDelegate, UITextFieldDe
         if ((locationInput.text?.isEmpty) != nil){
             locationInput.placeholder = locationPH
         }
+        
+        
             
         
     
