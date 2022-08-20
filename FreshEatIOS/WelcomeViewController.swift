8d0c1d6dbc29b469918b6cf2b374d13fd9432e53
Shimon <shimoco7@cs.colman.ac.il>
2022-08-20 17:24:41 +0300

    Flow after Splash View

--- FreshEatIOS/WelcomeViewController.swift
+++ FreshEatIOS/WelcomeViewController.swift
@@ -11,28 +11,6 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.startLoading()
-        let seconds = 2.0
-        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
-            Model.instance.isUserLoggedIn { email in
-                if email != nil {
-                Model.instance.isUserExists(email: email!) { success in
-                    if !success{
-                        Model.instance.signOut { success in
-                            self.stopLoading()
-                            print("Logout successfully - Navigating to Welcome Screen")
-                        }
-                    }
-                    else{
-                        self.performSegue(withIdentifier: "toHomeSegue", sender: nil)
-                       }
-                    }
-                }
-                else{
-                    self.stopLoading()
-                }
-            }
-        }
     }
 }
 
