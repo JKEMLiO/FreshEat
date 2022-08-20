81cb91b997b09d8f0b6007e722c25ce2bd4229d0
Shimon <shimoco7@cs.colman.ac.il>
2022-08-20 18:05:49 +0300

    Post Details - watch details according to post

--- FreshEatIOS/SeePostViewController.swift
+++ FreshEatIOS/SeePostViewController.swift
@@ -13,14 +13,18 @@ class SeePostViewController: UIViewController {
     @IBOutlet weak var namePostUIView: UIView!
     @IBOutlet weak var descriptionUIView: UIView!
     @IBOutlet weak var contactUIView: UIView!
+    @IBOutlet weak var titleTxt: UILabel!
     @IBOutlet weak var emailTxt: UITextView!
+    @IBOutlet weak var nameTxt: UITextView!
+    @IBOutlet weak var locationTxt: UITextView!
+    @IBOutlet weak var descriptionTxt: UITextView!
     @IBOutlet weak var userImg: UIImageView!
     @IBOutlet weak var editBtn: UIButton!
     @IBOutlet weak var deleteBtn: UIButton!
     
     var post:Post?{
         didSet{
-            if(namePostUIView != nil){
+            if(nameTxt != nil){
                 updateDisplay()
             }
         }
@@ -45,20 +49,43 @@ class SeePostViewController: UIViewController {
     }
     
     func updateDisplay(){
-      //Update display after repairing post details
+        titleTxt.text = post?.title
+        nameTxt.text = post?.username
+        locationTxt.text = post?.location
+        descriptionTxt.text = post?.postDescription
+        
+        if let url = post?.photo{
+            if !url.elementsEqual("vegImg"){
+                let u = URL(string:url)
+                postImg.kf.setImage(with: u)
+            }
+            else{
+                postImg.image = UIImage(named: "vegImg")
+            }
+        }
+        
+        Model.instance.getCurrentUser { currentUser in
+            if currentUser != nil{
+                Model.instance.getUser(byEmail: (self.post?.contactEmail)!) { user in
+                    if currentUser?.email != user!.email{
+                        self.editBtn.isHidden = true
+                        self.deleteBtn.isHidden = true
+                    }
+                    self.emailTxt.text = user!.email
+                    
+                    if (!(user!.avatarUrl!.elementsEqual("farmerAvatarSmall"))){
+                        let url = URL(string: user!.avatarUrl!)
+                        self.userImg?.kf.setImage(with: url)
+                    }else{
+                        self.userImg.image = UIImage(named: "farmerAvatarSmall")
+                    }
+                }
+            }
+        }
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
