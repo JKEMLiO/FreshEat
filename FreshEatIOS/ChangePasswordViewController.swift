9386579942bb2632015f1671c03f2ae523085e61
Shimon <shimoco7@cs.colman.ac.il>
2022-08-20 18:35:31 +0300

    updated properties to ChangePassword VC

---
+++ FreshEatIOS/ChangePasswordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChangePasswordViewController.swift
+//  FreshEatIOS
+//
+//  Created by Shiri Shmuely on 20/08/2022.
+//
+
+import UIKit
+
+class ChangePasswordViewController: UIViewController {
+
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var confirmPasswordInput: UITextField!
+    
+    @IBAction func changePassword(_ sender: Any){
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
