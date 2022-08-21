86666422e5f6c7f7a04aeec1300c3ab7faee0553
EMLIOJK <emil@cs.colman.ac.il>
2022-08-21 13:20:12 +0300

    Edit post view controller

---
+++ FreshEatIOS/EditPostViewController.swift
@@ -0,0 +1,73 @@
+//
+//  EditPostViewController.swift
+//  FreshEatIOS
+//
+//  Created by Emil Kollek on 21/08/2022.
+//
+
+import UIKit
+
+class EditPostViewController: UIViewController {
+    
+
+    @IBOutlet weak var titleTxt: UITextField!
+    @IBOutlet weak var mainTxt: UITextView!
+    @IBOutlet weak var locationTxt: UITextField!
+    @IBOutlet weak var phoneTxt: UITextField!
+    @IBOutlet weak var postImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleTxt.layer.cornerRadius=10
+        titleTxt.clipsToBounds = true
+        mainTxt.layer.cornerRadius = 10
+        mainTxt.clipsToBounds = true
+        locationTxt.layer.cornerRadius = 10
+        locationTxt.clipsToBounds = true
+        phoneTxt.layer.cornerRadius = 10
+        phoneTxt.clipsToBounds = true
+        postImg.layer.cornerRadius = 10
+        postImg.clipsToBounds = true
+
+        
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentTxt = textField.text ?? ""
+        guard let stringRange = Range(range, in: currentTxt) else {
+            return false
+        }
+        
+        let updateText = currentTxt.replacingCharacters(in: stringRange, with: string)
+        return updateText.count < 20
+    
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentTxt = textView.text ?? ""
+        guard let textRange = Range(range, in: currentTxt) else {
+            return false
+        }
+        
+        let updateText = currentTxt.replacingCharacters(in: textRange, with: text)
+        return updateText.count < 200
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "What Do You Offer?"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+    
+
+    @IBAction func restoreBtn(_ sender: Any) {
+    }
+    
+    @IBAction func changeImgBtn(_ sender: Any) {
+    }
+    
+    @IBAction func uploadBtn(_ sender: Any) {
+    }
+}
